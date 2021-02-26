247ce8604d9d39156a5c556b30cb81e5b8f9b3ce
Brandon Pfeifer <bnpfeife@gmail.com>
2021-02-26 12:11:43 -0500

    Update user.js repository and change user.js options.

--- firefox/user-overrides.js
+++ firefox/user-overrides.js
@@ -1,8 +1,3 @@
-/** GEOLOCATION ***/
-/* 0201: disable Location-Aware Browsing
- * [NOTE] Best left at default "true", fingerprintable, is already behind a prompt (see 0202)
- * [1] https://www.mozilla.org/firefox/geolocation/ ***/
-   // user_pref("geo.enabled", false);
-user_pref("geo.enabled", false);
-
 user_pref("layers.acceleration.force-enabled", true);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("signon.rememberSignons", false);
